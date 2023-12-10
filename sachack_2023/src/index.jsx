abb385446e8c6d7094b2cb2269a556b252bdc321
cdub-616 <cdub616@aol.com>
2023-12-09 19:01:42 -0800

    fixed issues

---
+++ sachack_2023/src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Index } from "./screens/Index/Index.jsx";
+
+const app = document.getElementById("app");
+const root = ReactDOM.createRoot(app);
+root.render(<Index />);
